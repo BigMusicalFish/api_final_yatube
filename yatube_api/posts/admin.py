878c806c6de5daa7d16d5a68bdd679aa2872a88c
Nikita Agapov <nikiton.89@yandex.ru>
2023-12-15 00:03:00 +0500

    first

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Comment, Group, Post, Follow 
+from .models import Comment, Group, Post, Follow
 
 
 @admin.register(Post)
@@ -14,4 +14,3 @@ class PostAdmin(admin.ModelAdmin):
 admin.site.register(Group)
 admin.site.register(Comment)
 admin.site.register(Follow)
-
